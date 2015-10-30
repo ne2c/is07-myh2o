f07cc29b3c5a7056f59284586b2c71d62d9ca2f0
5003 <msb5003@outlook.jp>
2015-10-30 17:16:34 +0900

    Init

---
+++ startup.sh
@@ -0,0 +1,9 @@
+for i in .rbenv:https://github.com/sstephenson/rbenv.git .rbenv/plugins:https://github.com/sstephenson/ruby-build.git; do
+  if ! [ -d ~/${i%%:*} ]; then
+    git clone ${i#*:} ~/${i%%:*}
+  else
+    pushd ~/${i%%:*}
+      git pull --no-ff
+    popd
+  fi
+done
