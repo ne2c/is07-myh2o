1a8a924a4df258cabc212c4cc42fd18703d29fde
5003 <msb5003@outlook.jp>
2015-11-02 17:08:41 +0900

    Update environment: Provisioning ('common' only)

--- startup.sh
+++ startup.sh
@@ -1,8 +1,15 @@
-for i in .rbenv:https://github.com/sstephenson/rbenv.git .rbenv/plugins:https://github.com/sstephenson/ruby-build.git; do
-  if ! [ -d ~/${i%%:*} ]; then
-    git clone ${i#*:} ~/${i%%:*}
+# Init
+for i in epel-release ansible; do
+  yum --quiet --assumeyes install $i
+done
+# Provisioning
+ansible-playbook /vagrant/playbooks/provisioning.yml
+# rbenv
+for j in .rbenv:https://github.com/sstephenson/rbenv.git .rbenv/plugins:https://github.com/sstephenson/ruby-build.git; do
+  if ! [ -d ~/${j%%:*} ]; then
+    git clone ${j#*:} ~/${j%%:*}
   else
-    pushd ~/${i%%:*} &&
+    pushd ~/${j%%:*} &&
       git pull --no-ff &&
     popd
   fi
