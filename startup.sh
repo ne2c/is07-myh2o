8b40382221e9e426b0bcf27d1de95d91a3d93fcb
5003 <msb5003@outlook.jp>
2015-11-12 00:29:42 +0900

    Provisioning: expressed as a relative path

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ for i in epel-release ansible; do
   sudo yum --quiet --assumeyes install $i
 done
 # Provisioning
-ansible-playbook /vagrant/playbooks/provisioning.yml
+ansible-playbook ./playbooks/provisioning.yml
 # rbenv
 for j in .rbenv:https://github.com/sstephenson/rbenv.git .rbenv/plugins/ruby-build:https://github.com/sstephenson/ruby-build.git; do
   if ! [ -d ~/${j%%:*} ]; then
