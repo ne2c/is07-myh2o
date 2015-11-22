26562a7739f258f4959c7172e88fa461988d3210
5003 <msb5003@outlook.jp>
2015-11-23 02:51:31 +0900

    ja: Init

--- startup.sh
+++ startup.sh
@@ -4,25 +4,3 @@ for i in epel-release ansible; do
 done
 # Provisioning
 ansible-playbook ./playbooks/provisioning.yml
-# rbenv
-for j in .rbenv:https://github.com/sstephenson/rbenv.git .rbenv/plugins/ruby-build:https://github.com/sstephenson/ruby-build.git; do
-  if ! [ -d ~/${j%%:*} ]; then
-    git clone ${j#*:} ~/${j%%:*}
-  else
-    pushd ~/${j%%:*} &&
-      git pull --no-ff &&
-    popd
-  fi
-done
-  # Profile
-  grep --quiet "rbenv init" ~/.bash_profile || cat ./__set_rbenv_profile.sh >> ~/.bash_profile && source ~/.bash_profile
-# gem
-echo "gem: --no-rdoc --no-ri" > ~/.gemrc
-# Ruby
-RB_VERSION="2.2.3"
-__install_ruby() {
-  rbenv install $RB_VERSION && rbenv global $RB_VERSION
-}
-# Setup
-rbenv versions | grep --quiet $RB_VERSION || __install_ruby
-gem list | grep --quiet bundler || gem install bundler
